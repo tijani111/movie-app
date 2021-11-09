592030d37022c82706ad5f0b442f7668ccd5d8c3
tijani111 <49434229+tijani111@users.noreply.github.com>
2021-11-09 15:18:23 +0100

    Add files via upload

--- src/Components/MovieList.js
+++ src/Components/MovieList.js
@@ -5,7 +5,7 @@ export default function MovieList({movieList,search,rate}) {
     return (
         <div className="card">
 
-            {movieList.filter(el=>el.titre.toUpperCase().includes(search.toUpperCase().trim())).map((el)=>(<MovieCard film={el}/>))}
+            {movieList.filter(el=>el.titre.toUpperCase().includes(search.toUpperCase().trim())& el.rate>=rate).map((el)=>(<MovieCard film={el}/>))}
         </div>
     )
 }
